21c467402d315cd13ac8aeae32a0ff031cbb5052
Rostislav Vitek <vitek@shopsys.cz>
2016-10-20 14:12:27 +0200

    gitlab issues export

---
+++ gitlab-issues-export.php
@@ -0,0 +1,19 @@
+<?php
+$xml = simplexml_load_file('setting.xml');
+$token = $xml->token;
+$ch = curl_init('http://gitlab/api/v3/projects/34/issues?milestone=New%20infrastructure&private_token=' . $token . '&per_page=100');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$output = curl_exec($ch);
+$issues = json_decode($output, true);
+$file = fopen('issues.csv', 'w');
+fputcsv($file, ['Název', 'Přiřazeno k', 'Stav']);
+foreach ($issues as $issue) {
+	$data = [
+		$issue['title'],
+		$issue['assignee']['name'],
+		$issue['state'],
+	];
+	fputcsv($file, $data);
+}
+fclose($file);
+curl_close($ch);
